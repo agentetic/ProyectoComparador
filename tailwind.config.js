4a18484054f51e0292748cdbcee0eb0977f24c13
Gyrkora <giselle.gallardo0210@gmail.com>
2022-01-14 15:13:04 -0500

    paleta de colores

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,40 @@
 module.exports = {
+  
+  
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  
+
   theme: {
-    extend: {},
+
+    colors: {
+   
+      //nav
+      hover: '#C8EFF8',
+      iconPerfil: '#4B94C1',
+      
+      
+      //hero redes sociales
+      facebook: '#226EBE',
+      instagram: '#F28907',
+      compartir: '#A9D941',
+      
+      
+      //Preguntas frecuenes
+      fondoSeccionPreguntas: '#9CC8D9',
+      colortitulo: '#2C3673', //se parece mucho al del footer
+
+      //formulario:
+      verdeBoton: '#78BF45',
+      tabs: '#4E98BD',
+      backgroundMain: '#F2F2F2',
+
+      //footer
+      moradoFooter: '#2D3073',
+    }
   },
+
+
   plugins: [],
 }
