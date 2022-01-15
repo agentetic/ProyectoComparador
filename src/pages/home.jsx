a8e86fb4e48cdd935061ad79275bd04059c7c20c
gabriel <gayagocr@gmail.com>
2022-01-14 19:30:32 -0600

    Finalizado

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Companies from '../components/Companies/Companies'
 import PreguntasFrecuentes from '../components/PreguntasFrecuentes/PreguntasFrecuentes'
-
+import Header from '../components/Header/Header'
 
 const Home = () => {
   return(
     <>
+      <Header/>
       <Companies />
       <PreguntasFrecuentes />    
     </>
